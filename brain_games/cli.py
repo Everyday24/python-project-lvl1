d487ca788fc821b61d60d28f4a979f5983a41923
Vitalii Vorobev <vorobev.vitalii24@gmail.com>
2020-08-09 14:54:52 +0300

    Update scripts, create setup

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -7,7 +7,3 @@ def welcome_user():
     """User`s name."""
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))
-
-extend-ignore =
-# we need print() several times
-WPS421
